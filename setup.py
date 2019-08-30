7810e9d9c20a26c527143ab0514ac3945d0beabc
Xiaoyong Guo <guo.xiaoyong@gmail.com>
2019-08-30 12:06:13 +0800

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 setup(name='merge_sshconf',
-      version='0.0.1',
+      version='0.0.2',
       description='merge multiple ssh config to one config',
       long_description='merge multiple ssh config to one config',
       author='Guo Xiaoyong',
       author_email='guo.xiaoyong@gmail.com',
       url='https://github.com/guoxiaoyong/merge_sshconf',
+      install_requires=['sshconf'],
+      setup_requires=['sshconf'],
       packages=['merge_sshconf'],
       include_package_data=True,
       entry_points={
